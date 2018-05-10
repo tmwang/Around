2567b4b2112c0f7cd1950a503e032092982239c6
tmwang <wangtianxiao89@gmail.com>
2018-05-09 23:49:22 -0500

    relocate files

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Header} from './header.js'
 import {Main} from './main.js'
-import './App.css';
+import '../styles/App.css';
 
 class App extends React.Component {
 	render() {
