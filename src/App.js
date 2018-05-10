e8f364dfcbf33874299b1fee5e463be3838cef39
tmwang <wangtianxiao89@gmail.com>
2018-05-09 22:58:55 -0500

    example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from './header.js'
+import {Main} from './main.js'
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				<Header/>
+				<Main/>
 			</div>
 		);
 	}
